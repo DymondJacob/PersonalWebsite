1bc4d339a7f4c3f8e39898a5d83cf937e233bdcb
Jacob Dymond <dymondjacob@gmail.com>
2023-05-13 14:33:23 -0400

    Project Setup

---
+++ app/projects/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Projects() {
+  return (
+    <main className="grid place-items-center font-mono md:pr-72 md:pl-72 sm:pr-4 sm:pl-4">
+      <div className="md:mt-24">
+        <h1>Coming Soon</h1>
+      </div>
+    </main>
+  );
+}
